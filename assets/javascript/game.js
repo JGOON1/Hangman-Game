bedbcc8fbe426ba344b9ea18ada8c010b33ecab3
john Meng <john.meng014@gmail.com>
2019-08-22 18:14:09 -0400

    have not made up progress since

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// Creating an array for themes
+var names = ["crank","handlebars","breaks","cog","frame","wheels","stem","saddle"];
+
+
+var selectname = names[Math.floor(Math.random() * names.length)];
+
+
+// setting up the correct answer/ with showing how many letters they have to guess
+var answer = [];
+for (var i = 0; i < names.length; i++){
+    answer = "_"
+};
+
+
+
+
+// // creating the function to start the game .
+// var start = function(){
+
+// // this selection on of the name form the array.
+
+// // pressing a key to play the game
+// document.onkeypress = function(k){
+// }
+
+// }
